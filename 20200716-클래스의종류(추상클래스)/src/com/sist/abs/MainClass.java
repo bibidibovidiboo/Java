59e86326ff912580b00a3929b07c376e75885169
SIST <SIST@192.168.10.232>
2020-07-16 17:29:40 +0900

    클래스의종류(추상클래스)

---
+++ 20200716-클래스의종류(추상클래스)/src/com/sist/abs/MainClass.java
@@ -0,0 +1,113 @@
+/*	< 추상 클래스  >
+ * 	 => 미완성된 클래스
+ * 	 => new를 사용해서 메모리 할당이 불가능
+ * 	 class A
+ * 		=> A a=new A();
+ * 	 abstract class B
+ * 		=> B b=new B(); ==> ERROR
+ * 	 => 항상 상속을 내려서 하위 클래스에서 구현한 다음에 사용
+ * 	 => 기본 틀이 존재
+ * 	 => 제시만 한다 : 구현하는 내용이 프로그램마다 다를 수도 있다
+ * 		예) 
+ * 			도형
+ * 
+ *   예)
+ *   	1. 게시판을 만들어라
+ *   	2. 게시판에는 글쓰기,내용보기,수정,삭제,찾기
+ *   
+ *   예) 
+ *   	영화관
+ *   	=======	
+ *   		영화목록
+ *   		예매
+ *   		영화 상세보기
+ *   		이벤트
+ *   		극장 설명
+ *   	===============
+ *   	cgv 메가박스 등등
+ *    
+ */
+package com.sist.abs;
+abstract class 도형 {
+	public abstract void draw();
+	public void color() {
+		System.out.println("검정색으로 그림을 그린다");
+	}
+}
+class 삼각형 extends 도형 {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("삼각형을 그린다");
+	}
+	// 정의 변경 => 재정의
+	
+	// override - color
+	@Override
+	public void color() {
+		// TODO Auto-generated method stub
+		System.out.println("빨간색으로 그림을 그린다");
+	}	
+	
+}
+class 원 extends 도형 {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("원을 그린다");
+	}
+	
+}
+class 사각형 extends 도형 {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("사각형을 그린다");
+	}
+	
+}
+public class MainClass {
+	public static void main(String[] args) {
+		// 도형 g=new 도형() => X
+		// 구현하는 얘의 생성자를 받는다 ★
+		도형 g=new 삼각형(); 
+			g.color();
+			g.draw();
+
+			g=new 사각형();
+			g.color();
+			g.draw();
+			
+			g=new 원();
+			g.color();
+			g.draw();
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 20200716-클래스의종류(추상클래스)/src/com/sist/abs/MainClass2.java
@@ -0,0 +1,119 @@
+/*  < 게시판  >
+ * 	= 답변형 게시판 => 답변하기
+ * 	= 댓글형 게시판 => 댓글달기
+ * 	= 갤러리 게시판 => 업로드
+ * 	= 자료실          => 업로드 , 다운로드
+ *	=========================== 추상클래스 , 인터페이스
+ *	글쓰기 ----------> 선언
+ *	내용보기
+ *	============================
+ *	수정
+ *	삭제 ------------> 동일 (구현)
+ *	찾기
+ *	============================ 
+ */
+package com.sist.abs;
+
+import javafx.scene.layout.Border;
+
+abstract class Board{
+	// 선언 (기능이 다 달라서 선언만 함)
+	public abstract void write();
+	public abstract void content(); 
+	
+	
+	public void update(){
+		System.out.println("게시물 수정");
+	}
+	public void delete(){
+		System.out.println("게시물 삭제");
+	}
+	public void find(){
+		System.out.println("게시물 찾기");
+	}
+	
+}
+// 상속
+class ReplyBoard extends Board {
+
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		System.out.println("답변형 게시판 글쓰기");
+	}
+
+	@Override
+	public void content() {
+		// TODO Auto-generated method stub
+		System.out.println("내용보기");
+	}
+	// 다른 기능 추가 가능하다
+	// Board가 가지고 있는 기능을 재정의가 가능
+}
+class DataBoard extends Board {
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		System.out.println("글쓰기+업로드");
+	}
+
+	@Override
+	public void content() {
+		// TODO Auto-generated method stub
+		System.out.println("내용보기+다운로드");
+	}
+}
+class GalleryBoard extends Board {
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		System.out.println("글쓰기+이미지업로드");
+	}
+
+	@Override
+	public void content() {
+		// TODO Auto-generated method stub
+		System.out.println("내용보기+이미지출력=>후기게시판");
+	}
+}
+class ReplyBoard2 extends Board {
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		System.out.println("글쓰기");
+	}
+
+	@Override
+	public void content() {
+		// TODO Auto-generated method stub
+		System.out.println("내용보기+댓글");
+	}
+}
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("===== 답변형 게시판 =====");
+		Board b=new ReplyBoard(); // new 뒤에 다름 => 추상클래스다
+		b.write();
+		b.content();
+		System.out.println("===== 갤러리 게시판 =====");
+		b=new GalleryBoard();
+		b.write();
+		b.content();
+		System.out.println("===== 자료실 =====");
+		b=new DataBoard();
+		b.write();
+		b.content();
+		System.out.println("===== 댓글 게시판 =====");
+		b=new ReplyBoard();
+		b.write();
+		b.content();
+	}
+}
+
+
+
+
+
+
