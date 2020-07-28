846dbc8fac0cb2259d736a352429552b3eba3fad
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2020-07-28 19:15:52 +0900

    Update MainClass.java

--- 20200615-자바기본/src/MainClass.java
+++ 20200615-자바기본/src/MainClass.java
@@ -1,15 +1,15 @@
-/*	< ÀÚ¹Ù ±âº»  >
- *   ÁÖ¼® Ã³¸® => ÇÁ·Î±×·¥À» ½ÇÇàÇÏÁö ¾Ê´Â´Ù 
- *   ¿©·¯ÁÙ ÁÖ¼® 
- *   // ÇÑÁÙ ÁÖ¼® 
- *   CTRL + SHIFT + / => ÁÖ¼®
- *   CTRL + SHIFT + \ => ÁÖ¼®ÇØÁ¦   
+/*	< ìë°” ê¸°ë³¸  >
+ *   ì£¼ì„ ì²˜ë¦¬ => í”„ë¡œê·¸ë¨ì„ ì‹¤í–‰í•˜ì§€ ì•ŠëŠ”ë‹¤ 
+ *   ì—¬ëŸ¬ì¤„ ì£¼ì„ 
+ *   // í•œì¤„ ì£¼ì„ 
+ *   CTRL + SHIFT + / => ì£¼ì„
+ *   CTRL + SHIFT + \ => ì£¼ì„í•´ì œ   
  */
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// µµ½ºÃ¢¿¡ Ãâ·ÂÇÒ ¶§ »ç¿ë 
+		// ë„ìŠ¤ì°½ì— ì¶œë ¥í•  ë•Œ ì‚¬ìš© 
 		  System.out.println("Hello Java"); 
 		  System.out.println("Hello Java");
 		  System.out.println("Hello Java"); 
@@ -17,4 +17,4 @@ public class MainClass {
 		  System.out.println("Hello Java");
 	}
 }
-// test
+// test page
