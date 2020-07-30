659afbe0ae9616996aac583daa9ce137cd67814f
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2020-07-30 22:33:16 +0900

    자바

--- 자바 프로젝트/src/com/sist/test/BookForm.java
+++
@@ -1,9 +0,0 @@
-// 도서추천 - 혜영
-package com.sist.test;
-import java.awt.*;
-import javax.swing.*;
-public class BookForm extends JPanel  {
-	public BookForm() {
-	setBackground(Color.white);  // 영역확인하려 넣어둔거니 수정할 때 지워여
-	}
-}
