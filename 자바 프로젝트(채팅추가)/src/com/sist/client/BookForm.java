fc79abda3b875cbcca7258768bb50ed0440749b6
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2020-08-04 17:07:00 +0900

    자바 프로젝트

--- 자바 프로젝트(채팅추가)/src/com/sist/client/BookForm.java
+++
@@ -1,9 +0,0 @@
-// 도서추천 - 혜영
-package com.sist.client;
-import java.awt.*;
-import javax.swing.*;
-public class BookForm extends JPanel  {
-	public BookForm() {
-	setBackground(Color.white);  // 영역확인용
-	}
-}
--- 자바 프로젝트(채팅추가)/src/com/sist/client/NewsForm.java
+++
@@ -1,9 +0,0 @@
-// 뉴스 - 임시
-package com.sist.client;
-import java.awt.*;
-import javax.swing.*;
-public class NewsForm extends JPanel  {
-	public NewsForm() {
-		setBackground(Color.red);  
-	}
-}
