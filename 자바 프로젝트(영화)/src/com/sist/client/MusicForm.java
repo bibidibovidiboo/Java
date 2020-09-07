df0c6eac85e32bccb809b875becf1eef61df0cb1
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2020-09-08 01:49:47 +0900

    자바 프로젝트

--- 자바 프로젝트(영화)/src/com/sist/client/MusicForm.java
+++
@@ -1,9 +0,0 @@
-// 음악추천 - 신진
-package com.sist.client;
-import java.awt.*;
-import javax.swing.*;
-public class MusicForm extends JPanel{
-	public  MusicForm() {
-		setBackground(Color.cyan); // 영역확인하려 넣어둔거니 수정할 때 지워여
-		}
-}
